ac362d7b44fe18c197bf0af95507f264813706ef
zcabh25@live.ucl.ac.uk <zcabh25@live.ucl.ac.uk>
2013-03-25 11:33:37 +0000

    Updated REST API to match ember js

--- OccupOSAPI/ServiceInterfaces/SensorsService.cs
+++ OccupOSAPI/ServiceInterfaces/SensorsService.cs
@@ -9,6 +9,7 @@ using System.Linq;
 using System.Web;
 using OccupOSAPI.Models;
 using System.Data.SqlClient;
+using ServiceStack.Text;
 
 namespace OccupOSAPI
 {
@@ -63,12 +64,18 @@ namespace OccupOSAPI
        public int URL { get; set; }
     }
 
+    public class JsonResp
+    {
+       public List<SensorDataResp> sensors { get; set; }
+    }
+
     public class SensorDataService : Service
     {
         Random rand = new Random();
         List<SensorData> resp,response;
         List<SensorDataResp> returnData;
         Dictionary<int, int> urls = null;
+        JsonResp tmpResp = new JsonResp();
         public object Get(SensorDataReq request)
         {         
             
@@ -135,8 +142,10 @@ namespace OccupOSAPI
                   value.measuredAt = tmp.MeasuredAt;
                   returnData.Add(value);
               }
-              return new HttpResult(returnData, ContentType.Json);
-             // return returnData;
+              tmpResp.sensors = returnData;
+              tmpResp.ToJson<JsonResp>();
+             // return new HttpResult(tmpResp, ContentType.Json);
+              return tmpResp;
           }
         }
     }
