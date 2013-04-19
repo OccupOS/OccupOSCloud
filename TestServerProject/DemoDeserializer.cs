af02490bab41f6669d2737084c8577933ead202e
Richard Davis <rdavis4200@gmail.com>
2013-04-19 20:29:59 +0100

    Added DemoDeserializer class

---
+++ TestServerProject/DemoDeserializer.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace OccupOSCloud
+{
+   public static class DemoDeserializer
+    {
+
+       /*
+         * DEMO DESERIALIZE METHODS
+         * Temporary (non-general) for the creation of SensorData objects with nodeID. 
+         */
+
+        public static ReturnPacket DeserializeJSON(string input) {
+            if (input != null && input != "{}") {
+                ReturnPacket packet = new ReturnPacket();
+                List<ReturnSensor> packetdata = new List<ReturnSensor>();
+                input = ReturnNextLayer(input);
+                packet.nodeID = int.Parse(GetIdElement(input));
+                string[] sensors = CommaSplit(ReturnNextLayer(input));
+                foreach (string sensor in sensors) {
+                    List<Position> posdata = new List<Position>();
+                    ReturnSensor data = new ReturnSensor();
+                    data.RefNum = int.Parse(GetIdElement(sensor));
+                    string[] readings = CommaSplit(ReturnNextLayer(sensor));
+                    foreach (string reading in readings) {
+                        string id = GetIdElement(reading);
+                        if (!id.Contains("1.")) {
+                            switch (id) {
+                                case ("a"): data.ReadTime = Convert.ToDateTime(GetDataElement(reading)); break;
+                                case ("b"): data.PollTime = Convert.ToDateTime(GetDataElement(reading)); break;
+                                case ("2"): data.SoundDb = int.Parse(GetDataElement(reading)); break;
+                                case ("3"): data.AnalogLight = int.Parse(GetDataElement(reading)); break;
+                                case ("4"): data.VibrationHz = int.Parse(GetDataElement(reading)); break;
+                                case ("5"): data.Humidity = int.Parse(GetDataElement(reading)); break;
+                                case ("7"): data.Pressure = int.Parse(GetDataElement(reading)); break;
+                                case ("8"): data.PowerWatt = int.Parse(GetDataElement(reading)); break;
+                                case ("9"): data.Temperature = int.Parse(GetDataElement(reading)); break;
+                                case ("10"): data.Windspeed = int.Parse(GetDataElement(reading)); break;
+                                default: break;
+                            }
+                        } else {
+                            Position position = new Position();
+                            string[] vals = CommaSplit(ReturnNextLayer(reading));
+                            position.X = int.Parse(GetDataElement(vals[0]));
+                            position.Y = int.Parse(GetDataElement(vals[1]));
+                            position.Depth = int.Parse(GetDataElement(vals[2]));
+                            posdata.Add(position);
+                        }
+                    }
+                    if (posdata.Count > 0) {
+                        Position[] posarray = new Position[posdata.Count];
+                        int pd_index = 0;
+                        foreach (Position pos in posdata) {
+                            posarray[pd_index] = pos;
+                            pd_index++;
+                        }
+                        data.EntityPositions = posarray;
+                    }
+                    packetdata.Add(data);
+                }
+                ReturnSensor[] senarray = new ReturnSensor[packetdata.Count];
+                int rs_index = 0;
+                foreach (ReturnSensor rsdata in packetdata) {
+                    senarray[rs_index] = rsdata;
+                    rs_index++;
+                }
+                packet.data = senarray;
+                return packet;
+            } else return null;
+        }
+
+        public static string ReturnNextLayer(string input) {
+                int startindex = input.IndexOf("{") + 1;
+                int length = input.LastIndexOf("}") - startindex;
+                if (startindex == -1 && length == -1) return input;
+                if (startindex == -1 || length == -1)
+                    throw new ArgumentException("Invalid JSON string format: brace not found");
+                input = input.Substring(startindex, length).Trim();
+                return input;
+            }
+
+        public static string[] CommaSplit(string input) {
+            int layer = 0;
+            int pos = 0;
+            List<string> temp = new List<string>();
+            foreach (char c in input) {
+                if (c == '{') layer++;
+                if (c == '}') layer--;
+                if (layer == 0 && c == ',') {
+                    temp.Add(input.Substring(0, pos));
+                    input = input.Substring(pos + 1, input.Length - (pos + 1));
+                    pos = -1;
+                }
+                    pos++;
+            }
+            temp.Add(input);
+            string[] result = new string[temp.Count];
+            pos = 0;
+            foreach (string str in temp) {
+                result[pos] = str;
+                pos++;
+            }
+            return result;
+        }
+
+        public static string GetIdElement(string input) {
+            input = input.Substring(0, input.IndexOf(':')).Trim();
+            input = input.Substring(1, input.Length - 2);
+            return input;
+        }
+
+        public static string GetDataElement(string input) {
+            input = input.Substring(input.IndexOf(':') + 1, input.Length - (input.IndexOf(':') + 1)).Trim();
+            if (input[0] == '\"' && input[input.Length - 1] == '\"')
+                input = input.Substring(1, input.Length - 2);
+            return input;
+        }
+    }
+
+    public class ReturnPacket {
+        public int nodeID;
+        public ReturnSensor[] data;
+    }
+
+    public class ReturnSensor {
+        public int RefNum;
+        public DateTime ReadTime;
+        public DateTime PollTime;
+        public float AnalogLight;
+        public int EntityCount;
+        public Position[] EntityPositions;
+        public float Humidity;
+        public float Pressure;
+        public float Temperature;
+        public float PowerWatt;
+        public float SoundDb;
+        public float VibrationHz;
+        public float Windspeed;
+    }
+
+    public struct Position {
+        public float Depth;
+        public int X;
+        public int Y;
+    }
+}
